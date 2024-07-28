708f2f562f47b48ef188a660f76de6c419ab65f4
Chuan Tran Hoang <chuan.th@teko.vn>
2024-07-28 17:12:32 +0700

    add key kb

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # productive-dev
+
+Hot key and tool for ubuntu (22.04)
--- keybinding/backup.bash
+++ keybinding/backup.bash
@@ -1,5 +1,8 @@
-# Set file setting keybinding
-gsettings list-recursively > settings.txt
-
 # Setting keybinding
 # https://askubuntu.com/questions/597395/how-to-set-custom-keyboard-shortcuts-from-terminal
+
+# All setting
+gsettings list-recursively > all_settings.txt
+
+# Custom keybinding
+
