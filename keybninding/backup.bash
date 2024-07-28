daa49a0d2231fcd4b96219cc09b5b9c9b2a74c69
Chuan Tran Hoang <chuan.th@teko.vn>
2024-07-28 07:18:24 +0700

    add key binding

---
+++ keybninding/backup.bash
@@ -0,0 +1,5 @@
+# Set file setting keybinding
+gsettings list-recursively > settings.txt
+
+# Setting keybinding
+# https://askubuntu.com/questions/597395/how-to-set-custom-keyboard-shortcuts-from-terminal
