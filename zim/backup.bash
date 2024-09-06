3ffe10f5ba9c675ded0c1957d2c5e1a659c4a8ba
Chuan Tran Hoang <chuan.th@teko.vn>
2024-09-06 11:00:08 +0700

    Setup tabtitle for zimtermtitle

---
+++ zim/backup.bash
@@ -0,0 +1,4 @@
+cp -r ~/.zim/ ./config/.zim/
+cp ~/.zimrc ./config/.zimrc
+cp ~/.p10k.zsh ./config/.p10k.zsh
+cp ~/.profile ./config/.profile
---
+++ zsh/backup.bash
@@ -0,0 +1,4 @@
+cp ~/.zshrc ./config/.zshrc
+cp ~/.zsh_aliases ./config/.zsh_aliases
+cp ~/.zsh_completion ./config/.zsh_completion
+cp ~/.zsh_kustom_func ./config/.zsh_kustom_func
