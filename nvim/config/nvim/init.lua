b5dad4c8e906db9439e640d13a1334cba9b6cbe5
Chuan Tran Hoang <chuan.th@teko.vn>
2024-09-19 17:32:00 +0700

    Update nvim config for telescope

--- nvim/config/nvim/init.lua
+++ nvim/config/nvim/init.lua
@@ -83,4 +83,38 @@ vim.api.nvim_create_autocmd("FileType", {
         vim.api.nvim_buf_set_keymap(0, 'n', '<CR>', ':lua jump_to_quickfix_entry()<CR>', { noremap = true, silent = true })
           end,
           }) 
+-- Function to search selected text in Telescope
+function SearchSelectedText()
+  -- Get the visually selected text
+  local selected_text = vim.fn.escape(vim.fn.getreg('v'), '\\') -- Get visual selection
+  require('telescope.builtin').grep_string({ search = selected_text })
+end
 
+-- Map '#' in visual mode to Telescope search
+vim.api.nvim_set_keymap('v', '#', ':lua SearchSelectedText()<CR>', { noremap = true, silent = true })
+-- Helper function to get the current visual selection
+function get_visual_selection()
+  local start_pos = vim.fn.getpos("'<")
+  local end_pos = vim.fn.getpos("'>")
+  local lines = vim.fn.getline(start_pos[2], end_pos[2])
+
+  if #lines == 0 then return '' end
+
+  -- Handle single-line selection
+  if #lines == 1 then
+    lines[1] = string.sub(lines[1], start_pos[3], end_pos[3])
+  else
+    -- Handle multi-line selection
+    lines[1] = string.sub(lines[1], start_pos[3])
+    lines[#lines] = string.sub(lines[#lines], 1, end_pos[3])
+  end
+
+  return table.concat(lines, '\n')
+end
+
+-- Function to search selected text in Telescope
+function SearchSelectedText()
+  local selected_text = get_visual_selection()  -- Get selected text
+  -- selected_text = vim.fn.escape(selected_text, '()[]{}^$.*+?|\\')
+  require('telescope.builtin').grep_string({ search = selected_text })
+end
