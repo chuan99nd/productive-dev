6eb9b02a3a31b7975f187b4cb2365988be056dcb
Chuan Tran Hoang <chuan.th@teko.vn>
2024-09-22 21:41:21 +0700

    update

--- nvim/config/nvim/lua/configs/bufferhelper.lua
+++ nvim/config/nvim/lua/configs/bufferhelper.lua
@@ -7,7 +7,7 @@ function close_other_buffers()
 
   for _, buf in ipairs(all_buffers) do
     if buf ~= current_buf and vim.api.nvim_buf_is_loaded(buf) then
-      vim.api.nvim_buf_delete(buf, { force = false })
+      vim.api.nvim_buf_delete(buf, { force = true })
     end
   end
 end
@@ -18,8 +18,8 @@ function close_buffers_to_the_right()
   local buffers = vim.fn.getbufinfo({ buflisted = 1 }) -- Get a list of all listed buffers
 
   for _, buf in ipairs(buffers) do
-    if buf.bufnr > current_bufnr then                       -- Check if buffer is to the right (higher number)
-      vim.api.nvim_buf_delete(buf.bufnr, { force = false }) -- Close buffer
+    if buf.bufnr > current_bufnr then                      -- Check if buffer is to the right (higher number)
+      vim.api.nvim_buf_delete(buf.bufnr, { force = true }) -- Close buffer
     end
   end
 end
